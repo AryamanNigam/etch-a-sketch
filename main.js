eb2cdbab72ebc6b7e1b1b95bc8c8a18e2332857c
Aryaman <aryamannig@gmail.com>
2024-09-26 03:18:52 +0530

    feature: added resizing

--- main.js
+++ main.js
@@ -1,13 +1,38 @@
 const container = document.querySelector("#container");
+const b = document.querySelector("button");
 
-for(let i =0; i < 256; i++){
-    let node = document.createElement("div");
-    node.classList.add("node");
-    container.appendChild(node);
-    node.addEventListener("mouseenter", () =>{
+function gen(num){
+    const style = getComputedStyle(container);
+    let h = parseInt(style.height, 10);
 
-        node.style.backgroundColor = "blue";
-    })
+    let n = h / num;
 
+
+    for(let i = 0; i < num * num; i++){
+        let node = document.createElement("div");
+        node.classList.add("node");
+
+        node.style.height = n + "px";
+        node.style.width = n + "px";
+        container.appendChild(node);
+
+        node.addEventListener("mouseenter", () =>{
+
+            node.style.backgroundColor = "blue";
+        })
+    }
 }
 
+gen(16);
+
+
+
+b.addEventListener("click", () => {
+
+    let num = prompt("Enter the number of squares per side for the new grid")
+    
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    gen(num);
+})
