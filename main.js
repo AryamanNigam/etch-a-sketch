c47141948c3668b1788b10fad00b99b49de3c4e3
Aryaman <aryamannig@gmail.com>
2024-09-27 02:15:35 +0530

    fix: fixed bugs

--- main.js
+++ main.js
@@ -1,25 +1,33 @@
 const container = document.querySelector("#container");
 const b = document.querySelector("button");
 
-function getRandomColor(){
+function getRandomColor() {
     let letters = "0123456789ABCDEF";
     let color = '#';
 
-    for(let i = 0; i < 6; i++){
-        color += letters[Math.floor(Math.random() * 16)]
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
     }
 
     return color;
 }
 
-function gen(num){
+let isMouseDown = false;
+
+document.addEventListener("mousedown", () => {
+    isMouseDown = true;
+});
+
+document.addEventListener("mouseup", () => {
+    isMouseDown = false;
+});
+
+function gen(num) {
     const style = getComputedStyle(container);
     let h = parseInt(style.height, 10);
-
     let n = h / num;
 
-
-    for(let i = 0; i < num * num; i++){
+    for (let i = 0; i < num * num; i++) {
         let node = document.createElement("div");
         node.classList.add("node");
 
@@ -27,23 +35,35 @@ function gen(num){
         node.style.width = n + "px";
         container.appendChild(node);
 
-        node.addEventListener("mouseover", () =>{
+        container.addEventListener("mouseleave", () => {
+            isMouseDown = false;
+        });
 
-            node.style.backgroundColor = getRandomColor();
-        })
+        let isColored = false; 
+
+        node.addEventListener("mousedown", () => {
+            if (!isColored) { 
+                node.style.backgroundColor = getRandomColor();
+                isColored = true; 
+            }
+        });
+
+        node.addEventListener("mousemove", () => {
+            if (isMouseDown && !isColored) {
+                node.style.backgroundColor = getRandomColor();
+                isColored = true; 
+            }
+        });
     }
 }
 
 gen(16);
 
-
-
 b.addEventListener("click", () => {
+    let num = prompt("Enter the number of squares per side for the new grid");
 
-    let num = prompt("Enter the number of squares per side for the new grid")
-    
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
     gen(num);
-})
+});
